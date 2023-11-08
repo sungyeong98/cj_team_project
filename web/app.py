64ec2bd2fe7b75d5ac547e5a587a8cf30e0bb847
YeJi-ing <ping_yeji@naver.com>
2023-11-09 02:25:52 +0900

    Feat: 웹 기능 추가

--- web/app.py
+++ web/app.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, render_template
 import os, sys
 
-# 현재 스크립트 파일의 디렉토리 경로를 얻음
-script_dir = os.path.dirname(__file__)
-relative_path = "/" 
-file_path = os.path.join(script_dir, relative_path)
-sys.path.append(file_path)
-import get_keyword
+#script_dir = os.path.dirname(__file__)
+#relative_path = "/" 
+#file_path = os.path.join(script_dir, relative_path)
+#sys.path.append(file_path)
+#import get_keyword
 
 app = Flask(__name__)
 
@@ -17,9 +16,15 @@ def hello():
 @app.route('/keyword', methods=['GET', 'POST'])
 def index():
     user_input = request.form.get('user_input')
-    result = get_keyword.GetKeyword(user_input)
 
-    return f'결과: {result}'
+    script_dir = os.path.dirname(__file__)
+    relative_path = "/" 
+    file_path = os.path.join(script_dir, relative_path)
+    sys.path.append(file_path)
+    import get_keyword
+    result = get_keyword.GetKeyword(user_input)
+    return render_template('page.html', message=result)
+    #return f'결과: {result}'
 
 if __name__ == '__main__':
     app.run(debug=True)
